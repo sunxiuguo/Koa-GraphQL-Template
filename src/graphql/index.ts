b5c26139071ce858d18452b923ebe5242504b1d6
sunxg <sunxg@fenbi.com>
2020-03-12 22:56:12 +0800

    ADD: build graphql schema and use ApolloServer middleware

---
+++ src/graphql/index.ts
@@ -0,0 +1,32 @@
+import path from 'path';
+import Koa from 'koa';
+import { ApolloServer } from 'apollo-server-koa';
+import { buildSchema } from 'type-graphql';
+
+function getResolvers() {
+    return [path.resolve(__dirname + '/resolvers/*.ts')];
+}
+
+function getSchema() {
+    return buildSchema({
+        resolvers: getResolvers()
+    });
+}
+
+export async function integrateGraphql(app: Koa<Koa.DefaultState, Koa.DefaultContext>) {
+    const server = new ApolloServer({
+        schema: await getSchema(),
+        playground: {
+            settings: {
+                'request.credentials': 'include'
+            }
+        } as any,
+        introspection: true,
+        context: ({ ctx }) => {
+            console.log(ctx);
+        }
+    });
+
+    server.applyMiddleware({ app });
+    return server;
+}
--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,24 @@
 // 引入模块
+import 'reflect-metadata';
 import Koa from 'koa';
 import KoaStatic from 'koa-static';
 import bodyParser from 'koa-bodyparser';
-
 import { connectDB } from './mongodb/index';
 import { router } from './router';
-// import { ApolloServer, gql } from 'apollo-server-koa';
+import { integrateGraphql } from './graphql';
 
 connectDB();
 
 const app = new Koa();
 
-// 使用 bodyParser 和 KoaStatic 中间件
-app.use(bodyParser());
-app.use(KoaStatic(__dirname + '/public'));
+integrateGraphql(app).then(server => {
+    // 使用 bodyParser 和 KoaStatic 中间件
+    app.use(bodyParser());
+    app.use(KoaStatic(__dirname + '/public'));
 
-app.use(router.routes()).use(router.allowedMethods());
+    app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000);
-
-console.log('graphQL server listen port: ' + 4000);
+    app.listen(4000, () => {
+        console.log(`server running success at ${server.graphqlPath}`);
+    });
+});

379b26ee3f4ad89ed8d01fec657c38418743fcd3
sunxg <sunxg@fenbi.com>
2020-03-16 16:18:31 +0800

    ADD:  mutation for student and info

--- src/graphql/resolvers/info.ts
+++ src/graphql/resolvers/info.ts
@@ -1,10 +1,28 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, InputType, Field } from 'type-graphql';
 import { InfoModal, Info } from '../../entities/info';
+
+@InputType()
+class InfoInput {
+    @Field(() => [String], { description: '爱好' })
+    public hobby!: string[];
+
+    @Field({ description: '身高' })
+    public height!: string;
+
+    @Field({ description: '体重' })
+    public weight!: number;
+}
 
 @Resolver(Info)
 export class InfoResolver {
     @Query(() => [Info], { nullable: true, description: '查询信息列表' })
     async infos() {
         return await InfoModal.find({});
+    }
+
+    @Mutation(() => Info)
+    async saveInfo(@Arg('data') newInfo: InfoInput) {
+        const info = new InfoModal(newInfo);
+        return await info.save();
     }
 }
--- src/graphql/resolvers/student.ts
+++ src/graphql/resolvers/student.ts
@@ -1,10 +1,32 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Mutation, Arg, InputType, Field } from 'type-graphql';
 import { StudentModel, Student } from '../../entities/student';
+
+@InputType()
+class StudentInput {
+    @Field({ description: '姓名', nullable: false })
+    public name!: string;
+
+    @Field({ description: '性别', nullable: false })
+    public sex!: string; // 考虑用enum
+
+    @Field({ description: '年龄', nullable: false })
+    public age!: number;
+
+    @Field({ description: 'infoid', nullable: false })
+    public info!: string;
+}
 
 @Resolver(Student)
 export class StudentResolver {
     @Query(() => [Student], { nullable: true, description: '查询学生列表' })
     async students() {
         return await StudentModel.find({});
+    }
+
+    @Mutation(() => Student)
+    async saveStudent(@Arg('data') newStudent: StudentInput) {
+        const student = new StudentModel(newStudent);
+        const res = await student.save();
+        return res;
     }
 }
